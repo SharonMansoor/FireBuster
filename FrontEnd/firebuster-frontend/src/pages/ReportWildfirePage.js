2f898f7507a6e469778db20d44585e9414e7feeb
Sharon Mansoor <sharonmans@gmail.com>
2020-04-14 13:35:00 +0300

    Report Wildfire

--- FrontEnd/firebuster-frontend/src/pages/ReportWildfirePage.js
+++ FrontEnd/firebuster-frontend/src/pages/ReportWildfirePage.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Grid } from "@material-ui/core";
+import ReportFireForm from "../components/ReportFireForm";
 
 class ReportWildFirePage extends Component {
   render() {
     return (
-        <div>
-          <h2>ReportWildFirePage</h2>
-        </div>
+      <Grid
+        container
+        direction="row"
+        alignItems="stretch"
+        style={{ height: "calc(100vh - 64px)" }}
+      >
+        <Grid
+          item
+          md={3}
+          style={{
+            overflowY: "auto",
+            height: "100%",
+            backgroundColor: "#eeeeee",
+          }}
+        >
+          <ReportFireForm/>
+        </Grid>
+        <Grid item md={9}></Grid>
+      </Grid>
     );
   }
 }
