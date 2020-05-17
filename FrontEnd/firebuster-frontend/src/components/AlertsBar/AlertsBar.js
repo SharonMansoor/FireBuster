e841f49aa252ec62bd0b05e65a5c7b386bf85433
Sharon Mansoor <sharonmans@gmail.com>
2020-05-17 18:18:16 +0300

    select alert

--- FrontEnd/firebuster-frontend/src/components/AlertsBar/AlertsBar.js
+++ FrontEnd/firebuster-frontend/src/components/AlertsBar/AlertsBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Box, Paper, Typography, Grid } from "@material-ui/core";
-import Alert from "./Alert";
+import { Box, Paper, Grid } from "@material-ui/core";
+import Alert from "../Alert";
+import './AlertsBar.css';
 
 class AlertsBar extends Component {
   render() {
@@ -10,11 +11,12 @@ class AlertsBar extends Component {
           {this.props.alerts.map((alert) => (
             <Grid item>
               <Paper
+              className={`alert ${this.props.currAlert.id==alert.id ? 'selectedAlert' : null}`}
                 elevation={3}
                 style={{
                   backgroundColor: "#fafafa",
                   padding: "5px",
-                  margin: "15px 15px 0px 15px",
+                  margin: "15px 15px 0px 15px"
                 }}
                 onClick={() => this.props.handleAlertClick(alert)}
               >
