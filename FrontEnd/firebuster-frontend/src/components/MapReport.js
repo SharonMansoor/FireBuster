c2fc5b1241188ed415b0ee63f3ce050591e054b0
Sharon Mansoor <sharonmans@gmail.com>
2020-06-17 18:10:38 +0300

    polygon in report page

--- FrontEnd/firebuster-frontend/src/components/MapReport.js
+++ FrontEnd/firebuster-frontend/src/components/MapReport.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
-import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
+import { Map, Marker, GoogleApiWrapper, Polygon } from "google-maps-react";
 
 export class MapReport extends Component {
+  calcLat = (lat) => {
+    return parseFloat(lat) + (1 / 111) * 2.5;
+  };
+
+  calcLat2 = (lat) => {
+    return parseFloat(lat) - (1 / 111) * 2.5;
+  };
+
   render() {
     return (
       <div style={{ height: "calc(100vh - 64px)", position: "relative" }}>
@@ -18,6 +26,34 @@ export class MapReport extends Component {
             <Marker
               position={this.props.location}
               icon="..\images\fireAlert.png"
+            />
+          )}
+          {this.props.location && (
+            <Polygon
+              key={this.props.id}
+              paths={[
+                {
+                  lat: this.calcLat(this.props.location.lat),
+                  lng: this.calcLat(this.props.location.lng),
+                },
+                {
+                  lat: this.calcLat(this.props.location.lat),
+                  lng: this.calcLat2(this.props.location.lng),
+                },
+                {
+                  lat: this.calcLat2(this.props.location.lat),
+                  lng: this.calcLat2(this.props.location.lng),
+                },
+                {
+                  lat: this.calcLat2(this.props.location.lat),
+                  lng: this.calcLat(this.props.location.lng),
+                },
+              ]}
+              strokeColor="#ff1900"
+              strokeOpacity={0.8}
+              strokeWeight={2}
+              fillColor="#ff1900"
+              fillOpacity={0.35}
             />
           )}
         </Map>
